93ef853ba43a836dc9bb0ba379ee06459f282c98
vovaushenko <vovaushenko1989@gmail.com>
2021-08-27 07:56:30 +0300

    all construct tabulation

---
+++ DP/17-all-construct-tabulation/allConstruct.js
@@ -0,0 +1,24 @@
+// allConstruct(abcdef, [ab, abc, cd , def, abcd, ef, c])
+
+//*TIME O(n^m) SPACE O(n^m) exponential
+
+const allConstruct = (target, wordBank) => {
+	const table = Array(target.length + 1)
+		.fill()
+		.map(() => []);
+
+	table[0] = [[]];
+
+	for (let i = 0; i < target.length; i++) {
+		for (let word of wordBank) {
+			if (target.slice(i, i + word.length) === word) {
+				const newCombinations = table[i].map((subArray) => [...subArray, word]);
+				table[i + word.length].push(...newCombinations);
+			}
+		}
+	}
+
+	return table[target.length];
+};
+
+console.log(allConstruct('purple', ['purp', 'p', 'ur', 'le', 'purp']));
