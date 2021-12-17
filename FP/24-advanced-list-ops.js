47f087bbfdcd0ea9c6059e7a8cd813418ad146d1
vovaushenko <vovaushenko1989@gmail.com>
2021-12-17 07:37:17 -0800

    FP-advanced-list-ops

--- FP/24-advanced-list-ops.js
+++ FP/24-advanced-list-ops.js
@@ -28,3 +28,47 @@ const flattenWithDepth = (arr, depth = Infinity) =>
 			),
 		[]
 	);
+
+// ================== ZIP=====================================
+/*
+zip( [1,3,5,7,9], [2,4,6,8,10] );
+ [ [1,2], [3,4], [5,6], [7,8], [9,10] ]
+*/
+
+function zip(arr1, arr2) {
+	var zipped = [];
+	arr1 = [...arr1];
+	arr2 = [...arr2];
+	while (arr1.length > 0 && arr2.length > 0) {
+		zipped.push([arr1.shift(), arr2.shift()]);
+	}
+	return zipped;
+}
+
+console.log(zip([1, 3, 5, 7, 9], [2, 4, 6, 8, 10]));
+// =================================================================
+
+// ================== Merge =====================================
+/*
+mergeLists( [1,3,5,7,9], [2,4,6,8,10] );
+ [1,2,3,4,5,6,7,8,9,10]
+*/
+
+const mergeLists = (arr1, arr2) => {
+	const merged = [];
+	arr1 = [...arr1];
+	arr2 = [...arr2];
+
+	while (arr1.length || arr2.length) {
+		if (arr1.length) merged.push(arr1.shift());
+		if (arr2.length) merged.push(arr2.shift());
+	}
+	return merged;
+};
+console.log(mergeLists([1, 3, 5, 7, 9], [2, 4]));
+// =================================================================
+
+// ================== Merge Reducer =====================================
+const mergeReducer = (merged, v, idx) =>
+	merged.slice(0, idx * 2).concat(v, merged.slice(idx * 2));
+console.log([1, 3, 5, 7, 9].reduce(mergeReducer, [2, 4, 6, 8, 10]));
