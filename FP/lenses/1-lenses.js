a1069139fbdf75bb1de1a710e7d96f7b3e84ce0e
vovaushenko <vovaushenko1989@gmail.com>
2021-12-19 18:57:08 -0800

    FP-lenses

---
+++ FP/lenses/1-lenses.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const getter = (prop) => (obj) => obj[prop];
+const setter = (prop) => (val, obj) => ({ ...obj, [prop]: val });
+
+const lens = (getter, setter) => ({
+	get: (obj) => getter(obj),
+	set: (val, obj) => setter(val, obj),
+});
+
+
+
+
+// use
+const nameLens = lens(getter('name'), setter('name'));
+
+console.dir({ nameLens });
--- FP/main.js
+++ FP/main.js
@@ -1,20 +1,7 @@
-// FP
-// 1 partial
+'use strict';
+const arr = [];
 
-const withDelay = (fn, delay) => {
-	return (...args) => {
-		const promise = new Promise((resolve, reject) => {
-			setTimeout(() => {
-				resolve(fn(...args));
-			}, delay);
-		});
-
-		return promise;
-	};
-};
-
-const sum = (a, b) => a + b;
-
-const delayedSum = withDelay(sum, 1000);
-
-console.log(delayedSum(2, 4).then((res) => console.log(res)));
+setInterval(() => {
+	arr.push(Math.floor(Math.random() * 100));
+	console.log(arr);
+}, 1000);
