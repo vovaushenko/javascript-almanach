87710d7624d300fe227ade94f3ff74c2aa9b07c3
vovaushenko <vovaushenko1989@gmail.com>
2021-12-28 09:27:26 -0800

    FP: memoize

--- FP/main.js
+++ FP/main.js
@@ -1,16 +1,3 @@
 'use strict';
 
-const pipe =
-	(...fns) =>
-	(x) =>
-		fns.reduce((v, f) => f(v), x);
-
-const addOne = (x) => x + 1;
-const square = (x) => x ** 2;
-const double = (x) => x * 2;
-const tripple = (x) => x * 3;
-
-const complicatedCalc = pipe(addOne, square, square, double, tripple);
-
-const res = complicatedCalc(2);
-console.log(res);
+// https://www.youtube.com/watch?v=PfuEfIiLX34&list=PLHhi8ymDMrQZGa7hYnwBQufg9XXoJr5LP&index=5
